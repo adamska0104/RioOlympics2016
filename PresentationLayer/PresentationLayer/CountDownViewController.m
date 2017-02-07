fcac98a23ba41fba7a8331ccd9d8c60c47d28c3b
adamska0104 <adamska@163.com>
2017-02-07 17:10:47 +0800

    PresentationLayer finished 1

--- PresentationLayer/PresentationLayer/CountDownViewController.m
+++ PresentationLayer/PresentationLayer/CountDownViewController.m
@@ -8,6 +8,46 @@
 
 #import "CountDownViewController.h"
 
+@interface CountDownViewController ()
+//显示倒计时
+@property (weak, nonatomic) IBOutlet UILabel *lblCountDown;
+@end
+
 @implementation CountDownViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //创建NSDateComponents对象
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    //设置NSDateComponents日期
+    [comps setDay:5];
+    //设置NSDateComponents月
+    [comps setMonth:8];
+    //设置NSDateComponents年
+    [comps setYear:2016];
+    //创建日历对象
+    NSCalendar *calender = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    //获得2016-8-5日的NSDate日期对象
+    NSDate *destinationDate = [calender dateFromComponents:comps];
+    //获得当前日期到2016-8-5时间的NSDateComponents对象
+    NSDateComponents *components = [calender components:NSCalendarUnitDay fromDate:[NSDate date] toDate:destinationDate options:NSCalendarWrapComponents];
+    //获得当前日期到2016-8-5相差的天数
+    NSInteger days = [components day];
+    
+    NSMutableAttributedString *strLabel = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%ld天",days]];
+    
+    [strLabel addAttribute:NSFontAttributeName
+                     value:[UIFont preferredFontForTextStyle:UIFontTextStyleFootnote]
+                     range:NSMakeRange(strLabel.length - 1, 1)];
+    
+    self.lblCountDown.attributedText = strLabel;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 
 @end
--- PresentationLayer/PresentationLayer/EventsDetailViewController.m
+++ PresentationLayer/PresentationLayer/EventsDetailViewController.m
@@ -10,4 +10,31 @@
 
 @implementation EventsDetailViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.imgEventIcon.image = [UIImage imageNamed:self.event.EventIcon];
+    
+    self.lblEventName.text = self.event.EventName;
+    self.txtViewBasicsInfo.text = self.event.BasicsInfo;
+    self.txtViewKeyInfo.text = self.event.KeyInfo;
+    self.txtViewOlympicInfo.text = self.event.OlympicInfo;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 @end
